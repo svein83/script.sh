771a1f83c9750c70aef5ee51c70b36faeba1b057
pigleet <25469452+svein83@users.noreply.github.com>
2021-04-22 00:42:05 +0200

    Update bash-substring-check.sh

--- bash-substring-check.sh
+++ bash-substring-check.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-instr(){ 
 # Pass 2 args for valid check.
 #   1. string to search inside
 #   2. string to find 
-#
- local HayStack="$1";
- local Needle="$2";
+
+function sting_in_string(){
+
+    local HayStack="$1"
+    local Needle="$2"
+    
     if [[ "$HayStack" != *"${Needle}"* ]]; then
-        echo \""$Needle"\"' is not in '\""$HayStack"\"'.';
+        echo \""$Needle"\"' is not in '\""$HayStack"\"'.'
     else
-        echo \""$Needle"\"' is in '\""$HayStack"\"'.';
+        echo \""$Needle"\"' is in '\""$HayStack"\"'.'
     fi
 }
